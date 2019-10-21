ad1c04699985d80bfb2cab96c112402a35bb326d
Kaushal Khamar <peacefulkaushalwin@gmail.com>
2019-10-21 09:25:43 +0530

    Initial Commit

---
+++ SimpleStudentPortal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleStudentPortal
+//
+//  Created by scsmac05 on 21/10/19.
+//  Copyright © 2019 Ahmedabad University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
